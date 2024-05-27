8ac460e25fb4384f20df58506d9c2aa603d1da6e
Jensooon <jenson.wintle@gmail.com>
2024-05-27 21:35:14 +0100

    Applied light and dark to footer

--- src/app/site-footer/site-footer.component.ts
+++ src/app/site-footer/site-footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ThemeServiceService } from '../theme-service.service';
 
 @Component({
   selector: 'app-site-footer',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './site-footer.component.html',
   styleUrl: './site-footer.component.css',
 })
-export class SiteFooterComponent {}
+export class SiteFooterComponent {
+  isComponentBGDark: boolean = false;
+  private themeSubscription?: Subscription;
+
+  constructor(private themeService: ThemeServiceService) {}
+
+  ngOnInit() {
+    this.themeSubscription = this.themeService
+      .getGlobalDark()
+      .subscribe((isDark) => {
+        this.isComponentBGDark = isDark;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
+    }
+  }
+}
