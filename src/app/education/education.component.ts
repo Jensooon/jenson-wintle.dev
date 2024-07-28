37be7151f27f7a8a22d00ef8c2b5c2c9d15c1f26
Jensooon <jenson.wintle@gmail.com>
2024-07-28 17:47:00 +0100

    Styled headings

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ThemeServiceService } from '../theme-service.service';
 
 @Component({
   selector: 'app-education',
   standalone: true,
   imports: [],
   templateUrl: './education.component.html',
-  styleUrl: './education.component.css'
+  styleUrl: './education.component.css',
 })
 export class EducationComponent {
+  isComponentBGDark: boolean = false;
+  private themeSubscription?: Subscription;
 
+  constructor(private themeService: ThemeServiceService) {}
+
+  ngOnInit() {
+    this.themeSubscription = this.themeService
+      .getGlobalDark()
+      .subscribe((isDark) => {
+        this.isComponentBGDark = isDark;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
+    }
+  }
 }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ThemeServiceService } from '../theme-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
   imports: [],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrl: './experience.component.css',
 })
 export class ExperienceComponent {
+  isComponentBGDark: boolean = false;
+  private themeSubscription?: Subscription;
 
+  constructor(private themeService: ThemeServiceService) {}
+
+  ngOnInit() {
+    this.themeSubscription = this.themeService
+      .getGlobalDark()
+      .subscribe((isDark) => {
+        this.isComponentBGDark = isDark;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
+    }
+  }
 }
