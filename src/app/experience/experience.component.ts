926b2f7759b764988ad2d2301deefcb29bcc9342
Jensooon <jenson.wintle@gmail.com>
2024-08-05 21:47:01 +0100

    Brought changes to Experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ThemeServiceService } from '../theme-service.service';
 import { Subscription } from 'rxjs';
+import { ScrollRevealDirective } from '../scroll-reveal.directive';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [ScrollRevealDirective],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css',
 })
