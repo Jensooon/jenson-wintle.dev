8e49053f07fb63d043d0f5815b01f6bf80124c13
Jensooon <jenson.wintle@gmail.com>
2024-09-02 19:53:51 +0100

    Implemented Icons

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { ThemeServiceService } from '../theme-service.service';
 import { Subscription } from 'rxjs';
 import { ScrollRevealDirective } from '../scroll-reveal.directive';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch, faDatabase } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [ScrollRevealDirective],
+  imports: [ScrollRevealDirective, FontAwesomeModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css',
 })
 export class ExperienceComponent {
+  faSearch = faSearch;
+  faDatabase = faDatabase;
   isComponentBGDark: boolean = false;
   private themeSubscription?: Subscription;
 
