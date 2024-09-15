b3baea6263c7bca73911d0560b7851d2fb6798fa
Jensooon <jenson.wintle@gmail.com>
2024-09-15 13:59:53 +0100

    Improved UI.

--- src/app/site-footer/site-footer.component.ts
+++ src/app/site-footer/site-footer.component.ts
@@ -12,6 +12,8 @@ import { ThemeServiceService } from '../theme-service.service';
 export class SiteFooterComponent {
   isComponentBGDark: boolean = false;
   private themeSubscription?: Subscription;
+  emailUrl: string = '/assets/mailDark.svg';
+  githubUrl: string = '/assets/githubDark.svg';
 
   constructor(private themeService: ThemeServiceService) {}
 
@@ -20,12 +22,29 @@ export class SiteFooterComponent {
       .getGlobalDark()
       .subscribe((isDark) => {
         this.isComponentBGDark = isDark;
+        this.changeEmailUrl();
+        this.changeGithubUrl();
       });
   }
 
   ngOnDestroy() {
     if (this.themeSubscription) {
       this.themeSubscription.unsubscribe();
+    }
+  }
+
+  changeEmailUrl() {
+    if (this.isComponentBGDark) {
+      this.emailUrl = '/assets/mailLight.svg';
+    } else {
+      this.emailUrl = '/assets/mailDark.svg';
+    }
+  }
+  changeGithubUrl() {
+    if (this.isComponentBGDark) {
+      this.githubUrl = '/assets/githubDark.svg';
+    } else {
+      this.githubUrl = '/assets/githubLight.svg';
     }
   }
 }
